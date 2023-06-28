e601a910aac444d554c4578fc6e2db7208497b11
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-06-28 13:50:01 +0300

    postgres v0.0.24

---
+++ backend/db/migrations/20230628104522-create-inputdb.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Inputdbs", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      body: {
+        type: Sequelize.TEXT,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      city: {
+        type: Sequelize.STRING,
+      },
+        authorId: {
+              type: Sequelize.INTEGER,
+              references: {
+                model: "Students",
+                key: "id",
+              },
+      },
+      image: {
+        type: Sequelize.TEXT,
+      },
+      video: {
+        type: Sequelize.BLOB,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Inputdbs');
+  }
+};
